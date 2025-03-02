c94b6483a9d19cc43cfd658734cf83a9b6ecab92
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-03-02 21:10:57 +0300

    update by linters

--- src/masks.py
+++ src/masks.py
@@ -9,5 +9,3 @@ def get_mask_card_number(card_number: Union[str]) -> str:
 def get_mask_account(account_number: Union[str]) -> str:
     """Функция принимает на вход номер счета в виде числа и возвращает маску **XXXX"""
     return f"**{account_number[-4:]}"
-
-
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(card_information: str) -> str:
     """Функция для обработки и маскировки информации о картах и счетах"""
@@ -17,6 +18,7 @@ def mask_account_card(card_information: str) -> str:
     else:
         return f"{name_for_mask} {get_mask_card_number(number_for_mask)}"
 
+
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
 print(mask_account_card("MasterCard 7158300734726758"))
@@ -26,9 +28,9 @@ print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
 
 
-def get_date (date: str) -> str:
+def get_date(date: str) -> str:
     """Функция для форматирования даты"""
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
-print(get_date("2024-03-11T02:26:18.671407"))
 
+print(get_date("2024-03-11T02:26:18.671407"))
