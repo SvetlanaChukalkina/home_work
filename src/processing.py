2a9477308840785e1fab500410cfa8d2a3824470
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-03-09 21:51:22 +0300

    add function sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+def filter_by_state(list_of_values: list, state: str="EXECUTED") -> list:
+    """Возвращает список словарей, у которых ключ state соответствует указанному значению"""
+    filter_list = []
+    for value in list_of_values:
+        if value["state"] == state:
+            filter_list.append(value)
+    return filter_list
+
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state="CANCELED"))
+
+
+def sort_by_date(list_of_values: list, reverse: bool= True) -> list:
+    """Возвращает список, отсортированный по дате"""
+    return sorted(list_of_values, key=lambda x: x["date"], reverse=reverse)
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
