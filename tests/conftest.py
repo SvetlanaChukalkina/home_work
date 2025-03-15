1f25c07ff24d71aab5749a527f74220d17d51b5c
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-03-15 14:32:30 +0300

    tests for masks

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture
+def empty_string():
+    return "Данные не введены"
+
+@pytest.fixture
+def not_correct_string():
+    return "Данные введены неверно"
+
+
+#@pytest.mark.parametrize("value, expected", [
+#
+#])
+
+
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,18 +1,29 @@
 from src.masks import get_mask_card_number, get_mask_account
 
-def test_card_number():
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
-    assert get_mask_card_number("700079228960636112121") == "Номер карты введен неверно"
-
-    assert get_mask_card_number("") == "Введите номер карты"
+def test_get_mask_card_number():
+    assert get_mask_card_number("") == "XXXX XX** **** XXXX"
 
 
-def test_account():
+def test_get_mask_card_number(empty_string):
+    assert get_mask_card_number("") == empty_string
+
+
+def test_get_mask_card_number(not_correct_string):
+    assert get_mask_card_number("700079228960636112121") == not_correct_string
+
+
+def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
 
-    assert get_mask_account("874305") == "**4305"
 
-    assert get_mask_account("") == "Введите номер счета"
+def test_get_mask_account(empty_string):
+    assert get_mask_account("") == empty_string
 
-    assert get_mask_account("4305") == "Номер счета введен неверно"
+
+def test_get_mask_account(not_correct_string):
+    assert get_mask_account("4305") == not_correct_string
+
+
+
+    
