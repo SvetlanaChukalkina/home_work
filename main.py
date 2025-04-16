11ea6c412950f648761380e3810e5106a830e1f1
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-04-16 23:15:40 +0300

    mail fixes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.utils import get_transactions_info
 from src.file_readers import csv_reader, excel_reader
+import re
+from src.operations import transactions_search
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
+from src.masks import get_mask_card_number, get_mask_account
 
 def main():
     user_input_file = input("""
@@ -28,29 +33,94 @@ def main():
                 print(f'Операции отфильтрованы по статусу {user_input_status.upper()}')
                 break
         user_input_sort = input("""Отсортировать операции по дате? Да/Нет
-        """).lower
+        """).lower()
         if user_input_sort == "да":
             user_input_range = input("""Отсортировать по возрастанию или по убыванию?
             По возрастанию / по убыванию
-            """)
+            """).lower()
         user_input_currency = input("""Выводить только рублевые тразакции? Да/Нет
-        """)
+        """).lower()
 
         user_input_description = input("""Отфильтровать список транзакций
         по определенному слову в описании? Да/Нет
-        """)
+        """).lower()
 
+        # Выбор формата исходного файла
         if user_choise == "JSON":
             initial_data = get_transactions_info(path="data/operations.json")
-            # print(initial_data)
         elif user_choise == "CSV":
             initial_data = csv_reader(path="data/transactions.csv")
-            print(initial_data)
-        # else:
-        #     initial_data = (функция чтения XLSX-словаря из папки data)
-        #
-        # print("Распечатываю итоговый список транзакций...")
-#
+        else:
+            initial_data = excel_reader(path="data/transactions_excel.xlsx")
+
+
+        #Фильтрация операций по выбранному слову в описании
+        filtered_transaction = filter_by_state(list_of_values=initial_data, state=user_input_status)
+
+        #Сортировка по дате
+        if user_input_sort == "да" and user_input_range == "по убыванию":
+            sorted_filtered_transaction = sort_by_date(list_of_values=filtered_transaction)
+        elif user_input_sort == "да" and user_input_range == "по возрастанию":
+            sorted_filtered_transaction = sort_by_date(list_of_values=filtered_transaction, reverse = False)
+        else:
+            sorted_filtered_transaction = filtered_transaction
+
+        #Выбор валюты операций
+        if user_input_currency == "да": #and "RUB" in sorted_filtered_transaction:
+            update_list = []
+            search_string = "RUB"
+            pattern = re.compile(search_string)
+            for i in sorted_filtered_transaction:
+                if pattern.findall(i['operationAmount']['currency']['code']):
+                    update_list.append(i)
+        elif user_input_currency == "нет":
+            update_list = sorted_filtered_transaction
+        # for up in update_list:
+        #     print(up)
+
+        #Поиск по ключевым словам в описании транзакции
+        if user_input_description == "нет":
+            final_list = update_list
+        elif user_input_description == "да":
+            word_input = input("""Введите ключевое слово для поиска
+            """)
+            final_list = transactions_search(operation_list=update_list, search_string=word_input)
+
+
+        #Вывод результатов
+        print("Распечатываю итоговый список транзакций...")
+
+        for fi in final_list:
+            unformatted_date = fi["date"]
+            formatted_date = get_date(unformatted_date)
+
+
+            operation_name = fi["description"]
+            operation_sum = fi['operationAmount']['amount'] + fi['operationAmount']['currency']['name']
+            sender = fi['from']
+            if "Счет" in sender:
+                mask_sender = get_mask_account(sender)
+            elif "Счет" not in sender:
+                mask_sender = mask_account_card(sender)
+            receiver = fi['to']
+            if "Счет" in receiver:
+                mask_receiver = get_mask_account(receiver)
+            elif "Счет" not in receiver:
+                mask_receiver = mask_account_card(receiver)
+
+
+            # print(fi)
+
+            if sender and receiver:
+                print(f"""{formatted_date}  {operation_name}, {mask_sender} -> {mask_receiver},
+                Сумма: {operation_sum}""")
+            elif sender and not receiver:
+                print(f"""{formatted_date}  {operation_name} {mask_sender},
+                Сумма: {operation_sum}""")
+            elif receiver and not sender:
+                print(f"""{formatted_date}  {operation_name} {mask_receiver},
+                Сумма: {operation_sum}""")
+
 #     print(f"""Всего банковских операций в выборке:
 
     # 08.12.2019 Открытие вклада 
@@ -72,19 +142,4 @@ def main():
 
 
 main()
-
-        # if user_input_status.upper() in ["EXECUTED", "CANCELED", "PENDING"]:
-        #     print(f'Операции отфильтрованы по статусу {user_input_status.upper()}')
-        #     filtered_transaction = []
-        #     pattern = re.compile(transaction_search_string=user_input_status, flags=re.IGNORECASE)
-        #     for init in initial_data:
-        #         if pattern.findall(init["description"]):
-        #             filtered_transaction.append(init)
-
-    # if user_input_sort == "да":
-    #     функция преобразования даты из модуля, посмотреть, как
-    #     подходит к исходным данным
-
-
-        # if user_input_range.lower() == "по убыванию":
-        #     посмотреть по функции
+#
--- src/processing.py
+++ src/processing.py
@@ -6,11 +6,11 @@ def filter_by_state(list_of_values: list[dict[Any, Any]], state: str = "EXECUTED
     """Возвращает список словарей, у которых ключ state соответствует указанному значению"""
     filter_list = []
     for value in list_of_values:
-        if value["state"] == state:
+        if "state" in value and value["state"] == state:
             filter_list.append(value)
     return filter_list
 
 
 def sort_by_date(list_of_values: list[dict], reverse: bool = True) -> list[dict]:
     """Возвращает список, отсортированный по дате"""
-    return sorted(list_of_values, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%d"), reverse=reverse)
+    return sorted(list_of_values, key=lambda x: datetime.strptime(x["date"][0:10], "%Y-%m-%d"), reverse=reverse)
